0d1b45e12049ccbf6090f3d80c64cd8a037979c2
Rebecca <rfjackson11@gmail.com>
2015-06-30 13:48:06 -0600

    update

--- koans/about_arrays.rb
+++ koans/about_arrays.rb
@@ -39,7 +39,7 @@ class AboutArrays < Neo::Koan
     assert_equal [:peanut, :butter], array[0,2]
     assert_equal [:and, :jelly], array[2,2]
     assert_equal [:and, :jelly], array[2,20]
-    assert_equal __, array[4,0]
+    assert_equal [], array[4,0]
     assert_equal __, array[4,100]
     assert_equal __, array[5,0]
   end
